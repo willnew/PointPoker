27e287b8cdac45935ea03c1c05c163fff2b78d70
crusoexia <xiawenlan1984@gmail.com>
2015-11-21 20:16:57 +0800

    server side basic setup

---
+++ src/test/domain/AttendeeSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+var should = require('should');
+var Attendee = require('../../main/domain/Attendee');
+
+describe('{Attendee}', function() {
+  describe('#constructor', function() {
+    it('should be able to instantiate.', function() {
+      var attendee = new Attendee();
+      attendee.should.not.be.null;
+      attendee.should.be.an.instanceof(Attendee);
+    });
+  });
+});
