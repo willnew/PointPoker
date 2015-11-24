e59a815a6358d78eef1f345578a064a6c0ef3fb6
crusoexia <xiawenlan1984@gmail.com>
2015-11-24 23:34:14 +0800

    add scaffold for websocket session access testing

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
+'use strict';
+
 // import libs
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
+var session = require('express-session');
 var bodyParser = require('body-parser');
-
 var app = express();
+var httpServer = require('http').Server(app);
+var wss = new (require('ws').Server)({ server: httpServer });
+
 app.set('serverIp', 'localhost');
 app.set('port', 10086);
 app.use('/', express.static(path.join(__dirname, 'web')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'keyboard cat'
+}));
 
 // variables
 var POINTS = path.join(__dirname, 'data.json');
@@ -31,9 +41,20 @@ app.get('/points', function(req, res) {
 app.post('/points', function(req, res) {
 });
 
+app.post('/rest/login', function(req, res) {
+  var sess = req.session;
+  console.log(req.sessionID);
+  sess.userName = req.body.name;
+  res.json({name: req.body.name});
+});
+
+// Websocket
+wss.on('connection', function(ws) {
+  console.log(ws.upgradeReq);
+});
 
 // broadcast server IP
-app.listen(app.get('port'), function() {
+httpServer.listen(app.get('port'), function() {
   console.log("You can broadcast this message to team members:\n");
   console.log("http://" + app.get('serverIp') + app.get('port'));
 });
